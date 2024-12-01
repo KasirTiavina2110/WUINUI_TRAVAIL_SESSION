02bc70c91c399682af88c24944122f02202e5e6c
OursonSmoothie <simon13pratte@hotmail.com>
2024-12-01 13:31:51 -0500

    Page Statistique
    J'ai créé un SingletonStatistique pour gerer les statistiques. J'ai ajouté les statistiques dans la page statistiques, le tout en procédure stockée

---
+++ Copie_Vierge/class2/SingletonStatistique.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace class2
+{
+    internal class SingletonStatistique
+    {
+
+        MySqlConnection con;
+        ObservableCollection<Activite> listeActivite;
+
+        public static SingletonStatistique instance = null;
+
+
+        public SingletonStatistique()
+        {
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2024_420-345-ri_eq15;Uid=1748697;Pwd=1748697;");
+            listeActivite = new ObservableCollection<Activite>();
+
+
+        }
+
+
+        public static SingletonStatistique GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SingletonStatistique();
+            }
+            return instance;
+        }
+
+        //Trouver le nombre d'adhérents
+        public int NombreAdherent()
+        {
+            int nombre = 0;
+
+            try
+            {
+
+                MySqlCommand commande = new MySqlCommand("nombre_total_adherent");
+
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+                commande.Prepare();
+                nombre = Convert.ToInt32(commande.ExecuteScalar());
+                con.Close();
+
+                return nombre;
+
+
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+
+                return nombre;
+            }
+
+        }
+
+        //Trouver le nombre d'activite
+
+        public int NombreActivite()
+        {
+            int nombre = 0;
+            try
+            {
+               
+                MySqlCommand commande = new MySqlCommand("nombre_total_activite");
+
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+                commande.Prepare();
+                nombre = Convert.ToInt32(commande.ExecuteScalar());
+                con.Close();
+
+                return nombre;
+
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+                return nombre;
+            }
+
+        }
+
+
+        public Dictionary<string, int> AdherentParActivite()
+        {
+            int nombre = 0;
+            string nomActivite = "";
+            var informations = new Dictionary<string, int>();
+
+            try
+            {
+
+                MySqlCommand commande = new MySqlCommand("adherent_par_activite");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+                MySqlDataReader reader = commande.ExecuteReader();
+                while (reader.Read())
+                {
+                    nombre = Int32.Parse(reader["NombreAdherent"].ToString());
+                    nomActivite = reader["numero_activite"].ToString();
+
+                    informations[nomActivite] = nombre; //Associer le nombre et la catégorie en ensemble
+
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                con.Close();
+            }
+            return informations; //Retourner le dictionnaire de données
+
+
+
+        }
+
+        public Dictionary<string, double> NoteMoyenneActivite()
+        {
+            double nombre = 0;
+            string nomActivite = "";
+            var informations = new Dictionary<string, double>();
+
+            try
+            {
+
+                MySqlCommand commande = new MySqlCommand("note_moyenne_par_activite");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+                MySqlDataReader reader = commande.ExecuteReader();
+                while (reader.Read())
+                {
+                    nombre = Double.Parse(reader["noteMoyenne"].ToString());
+                    nomActivite = reader["numero_activite"].ToString();
+
+                    informations[nomActivite] = nombre; //Associer le nombre et la catégorie en ensemble
+
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                con.Close();
+            }
+
+            return informations; //Retourner le dictionnaire de données
+
+        }
+
+
+        public Dictionary<string, int> NombreInscriptionParType()
+        {
+
+            int nombre = 0;
+            string typeActivite = "";
+            var informations = new Dictionary<string, int>();
+
+            try
+            {
+
+                MySqlCommand commande = new MySqlCommand("nombre_inscription_par_type");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+                MySqlDataReader reader = commande.ExecuteReader();
+                while (reader.Read())
+                {
+                    nombre = Int32.Parse(reader["inscriptionParType"].ToString());
+                    typeActivite = reader["type"].ToString();
+
+                    informations[typeActivite] = nombre; //Associer le nombre et la catégorie en ensemble
+
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                con.Close();
+            }
+            return informations; //Retourner le dictionnaire de données
+
+        }
+
+        public double AgeMoyenAdherent()
+        {
+
+            double ageMoyen = 0;
+            try
+            {
+
+                MySqlCommand commande = new MySqlCommand("age_moyen_adherent");
+
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+                commande.Prepare();
+                ageMoyen = Convert.ToDouble(commande.ExecuteScalar());
+                con.Close();
+
+                return ageMoyen;
+
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                return ageMoyen;
+            }
+
+        }
+
+
+        public Dictionary<string, int> ActivitePlusAdherent()
+        {
+
+            int nombre = 0;
+            string numeroActivite = "";
+            var informations = new Dictionary<string, int>();
+
+            try
+            {
+
+                MySqlCommand commande = new MySqlCommand("activite_plus_adherent");
+                commande.Connection = con;
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                con.Open();
+                MySqlDataReader reader = commande.ExecuteReader();
+                while (reader.Read())
+                {
+                    nombre = Int32.Parse(reader["nombreAdherent"].ToString());
+                    numeroActivite = reader["numero_activite"].ToString();
+
+                    informations[numeroActivite] = nombre; //Associer le nombre et la catégorie en ensemble
+
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                con.Close();
+            }
+            return informations; //Retourner le dictionnaire de données
+
+        }
+
+    }
+}
--- Copie_Vierge/class2/VoirStatistique.xaml.cs
+++ Copie_Vierge/class2/VoirStatistique.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -7,11 +8,13 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Text;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +29,176 @@ namespace class2
         public VoirStatistique()
         {
             this.InitializeComponent();
+            AfficherNombreAdherent();
+            AfficherNombreActivite();
+            AdherentParActivite();
+            MoyenneEvaluationParActivite();
+            NombreInscriptionParType();
+            AgeMoyenAdherent();
+            ActivitePlusAdherent();
         }
+
+        public void AfficherNombreAdherent()
+        {
+
+            int nombreAdherent = SingletonStatistique.GetInstance().NombreAdherent();
+            nombreTotalAdherents.Text += nombreAdherent.ToString();
+
+
+        }
+
+        public void AfficherNombreActivite()
+        {
+
+            int nombreActivite = SingletonStatistique.GetInstance().NombreActivite();
+            nombreTotalActivite.Text += nombreActivite.ToString();
+
+        }
+
+        public void AdherentParActivite()
+        {
+            var informations = SingletonStatistique.GetInstance().AdherentParActivite();
+
+            stk_parent.Children.Clear(); // Vider le StackPanel avant d'ajouter les nouveaux éléments
+
+            //Entête
+            TextBlock header = new TextBlock 
+            { 
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(0, 10, 0, 0),
+                FontWeight = FontWeights.Bold,
+                TextDecorations = TextDecorations.Underline,
+                FontSize = 25, Text = "Le nombre d'adhérent par séance"
+            }; 
+            stk_parent.Children.Add(header);
+
+            foreach (var info in informations)
+            {
+                StackPanel seanceStackPanel = new StackPanel
+                {
+                    Margin = new Thickness(15),
+                    Orientation = Orientation.Horizontal,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                TextBlock seanceText = new TextBlock
+                {
+                    Text = $"Activité {info.Key} : {info.Value} adhérents"
+                };
+
+                seanceStackPanel.Children.Add(seanceText);
+                stk_parent.Children.Add(seanceStackPanel);
+            }
+          
+        }
+
+        public void MoyenneEvaluationParActivite()
+        {
+
+            var informations = SingletonStatistique.GetInstance().NoteMoyenneActivite();
+
+            stk_note_parent.Children.Clear();
+
+            //Entête
+            TextBlock header = new TextBlock
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(0, 10, 0, 0),
+                FontWeight = FontWeights.Bold,
+                TextDecorations = TextDecorations.Underline,
+                FontSize = 25,
+                Text = "Le nombre d'adhérent par séance"
+            };
+
+            stk_note_parent.Children.Add(header);
+
+            foreach (var info in informations)
+            {
+                StackPanel seanceStackPanel = new StackPanel
+                {
+                    Margin = new Thickness(15),
+                    Orientation = Orientation.Horizontal,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                TextBlock seanceText = new TextBlock
+                {
+                    Text = $"Activité {info.Key} : ",
+                    Margin = new Thickness(5, 5, 0, 0)
+
+                };
+
+                RatingControl rating = new RatingControl
+                {
+                    Value = info.Value,
+                    IsReadOnly = true,
+                    Margin = new Thickness(5, 0, 0, 0)
+                };
+
+                seanceStackPanel.Children.Add(seanceText);
+                seanceStackPanel.Children.Add(rating);
+                stk_note_parent.Children.Add(seanceStackPanel);
+            }
+        }
+
+
+        //Voir le nombre d'inscription par type
+        public void NombreInscriptionParType()
+        {
+            var informations = SingletonStatistique.GetInstance().NombreInscriptionParType();
+
+            stk_type_parent.Children.Clear();
+
+            //Entête
+            TextBlock header = new TextBlock
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(0, 10, 0, 0),
+                FontWeight = FontWeights.Bold,
+                TextDecorations = TextDecorations.Underline,
+                FontSize = 25,
+                Text = "Le nombre d'inscription par type"
+            };
+
+            stk_type_parent.Children.Add(header);
+
+            foreach (var info in informations)
+            {
+                StackPanel seanceStackPanel = new StackPanel
+                {
+                    Margin = new Thickness(15),
+                    Orientation = Orientation.Horizontal,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                TextBlock seanceText = new TextBlock
+                {
+                    Text = $"{info.Key} : {info.Value} inscriptions",
+
+                };
+
+                seanceStackPanel.Children.Add(seanceText);
+                stk_type_parent.Children.Add(seanceStackPanel);
+            }
+        }
+
+        public void AgeMoyenAdherent()
+        {
+            var informations = SingletonStatistique.GetInstance().AgeMoyenAdherent();
+            AgeMoyen.Text += informations.ToString();
+
+        }
+
+        public void ActivitePlusAdherent()
+        {
+            var informations = SingletonStatistique.GetInstance().ActivitePlusAdherent();
+
+            foreach(var info in informations)
+            {
+                activitePlusPopulaire.Text = $"{info.Key} : {info.Value}";
+            }
+
+        }
+
     }
 }
